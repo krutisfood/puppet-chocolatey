69b4ab7a1f998b6a87ebaab3c5910d0d1a603369
Mathew Finch <M.Finch@nib.com.au>
2014-03-26 10:31:06 +1100

    Added require for any time chocolatey provider is used, begin initial work on templating config file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,15 @@
 # This class will install and configure chocolatey
 # Chocolatey needs to be installed to work with the provider in this module
 class chocolatey { 
-  exec { 'install-chocolatey':
-    creates => 'C:\Chocolatey',
-    command => 'cmd.exe /K @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString(\'https://chocolatey.org/install.ps1\'))"'
-  }
+  exec { 
+    'install-chocolatey':
+      creates => 'C:\Chocolatey',
+      command => 'cmd.exe /K @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString(\'https://chocolatey.org/install.ps1\'))"'
+  } -> Package <| provider == chocolatey |>
+  
+#  file {
+#    'C:/Chocolatey/chocolatey.config':
+#      ensure  => present,
+#      content => template('chocolatey/chocolatey.config.erb');
+#  }
 }
