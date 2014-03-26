246159703c5a3dfa237c82d86adb0880a4fddb39
Mathew Finch <M.Finch@nib.com.au>
2014-03-26 11:32:37 +1100

    Added ability to configure sources in the chocolatey config file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 # This class will install and configure chocolatey
 # Chocolatey needs to be installed to work with the provider in this module
-class chocolatey { 
+class chocolatey (
+  $choc_sources
+  ){ 
   exec { 
     'install-chocolatey':
       creates => 'C:\Chocolatey',
@@ -8,9 +10,9 @@ class chocolatey {
       command => 'cmd.exe /K @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString(\'https://chocolatey.org/install.ps1\'))"'
   } -> Package <| provider == chocolatey |>
   
-#  file {
-#    'C:/Chocolatey/chocolatey.config':
-#      ensure  => present,
-#      content => template('chocolatey/chocolatey.config.erb');
-#  }
+  file {
+    'C:/Chocolatey/chocolateyinstall/chocolatey.config':
+      ensure  => present,
+      content => template('chocolatey/chocolatey.config.erb');
+  }
 }
