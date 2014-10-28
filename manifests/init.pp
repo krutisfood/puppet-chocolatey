500360aacc5670054b78fb0398486878c0de1cf0
Kurt Gardiner <k.a.gardiner@gmail.com>
2014-10-29 09:34:13 +1100

    Update install in init.pp to handle new version of chocolatey

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,13 +5,13 @@ class chocolatey (
   ){ 
   exec { 
     'install-chocolatey':
-      creates => 'C:\Chocolatey',
+      creates => 'C:\ProgramData\Chocolatey',
       path    => $::path,
       command => 'cmd.exe /K @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString(\'https://chocolatey.org/install.ps1\'))"'
   } -> Package <| provider == chocolatey |>
   
   file {
-    'C:/Chocolatey/chocolateyinstall/chocolatey.config':
+    'C:/ProgramData/Chocolatey/chocolateyinstall/chocolatey.config':
       ensure  => present,
       content => template('chocolatey/chocolatey.config.erb');
   }
