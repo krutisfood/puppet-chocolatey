17e4ea12792a2f68fb577b936766f8a1f2116a01
Hailey Martin <h.martin@nib.com.au>
2015-04-13 14:55:28 +1000

    Update installation to work with new chocolatey (and force update if it's the old one)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,29 @@
 # This class will install and configure chocolatey
 # Chocolatey needs to be installed to work with the provider in this module
 class chocolatey (
-  $choc_sources
-  ){ 
+  $sources  = {},
+  $features = {}
+  ){
+  $chocolatey_path = 'C:\ProgramData\chocolatey'
+
+  # Will reinstall Chocolatey if the host is running the old Powershell-based version.
   exec { 
     'install-chocolatey':
-      creates => 'C:\ProgramData\Chocolatey',
-      path    => $::path,
-      command => 'cmd.exe /K @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString(\'https://chocolatey.org/install.ps1\'))"'
+      path     => $::path,
+      provider => powershell,
+      command  => 'iex ((new-object net.webclient).DownloadString(\'https://chocolatey.org/install.ps1\'))',
+      unless   => "if (!(Test-Path ${chocolatey_path}) -or ((choco -v) -match '0.9.8')) { exit 1 } else { exit 0 }"
   } -> Package <| provider == chocolatey |>
-  
+
+  $default_features = {
+    'checksum_files'            => true,
+    'auto_uninstaller'          => false,
+    'allow_global_confirmation' => false
+  }
+  $_features         = merge($default_features, $features)
+
   file {
-    'C:/ProgramData/Chocolatey/chocolateyinstall/chocolatey.config':
+    "${chocolatey_path}/config/chocolatey.config":
       ensure  => present,
       content => template('chocolatey/chocolatey.config.erb');
   }
