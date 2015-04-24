74d6af451481e66c9d4c717503bc20677bf89ee3
Hailey Martin <h.martin@nib.com.au>
2015-04-24 14:29:42 +1000

    Custom install script to lock version to 0.9.8 and no longer manually manage config file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,40 @@
 # This class will install and configure chocolatey
 # Chocolatey needs to be installed to work with the provider in this module
 class chocolatey (
-  $choc_sources
-  ){ 
+  $sources = { 'chocolatey' => 'https://chocolatey.org/api/v2/' }
+  ){
+
+  file { 'C:/temp':
+    ensure => directory,
+  }
+
+  file { 'C:/temp/install.ps1':
+    ensure             => present,
+    source             => 'puppet:///modules/chocolatey/install.ps1',
+    source_permissions => 'ignore',
+  }
+
+  file { 'C:/temp/set-sources.ps1':
+    ensure             => present,
+    content            => template('chocolatey/set-sources.ps1.erb'),
+    source_permissions => 'ignore',
+  }
+
   exec { 
     'install-chocolatey':
-      creates => 'C:\ProgramData\Chocolatey',
+      creates => 'C:/ProgramData/Chocolatey',
       path    => $::path,
-      command => 'cmd.exe /K @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString(\'https://chocolatey.org/install.ps1\'))"'
-  } -> Package <| provider == chocolatey |>
-  
-  file {
-    'C:/ProgramData/Chocolatey/chocolateyinstall/chocolatey.config':
-      ensure  => present,
-      content => template('chocolatey/chocolatey.config.erb');
+      command => 'cmd.exe /K @powershell -NoProfile -ExecutionPolicy unrestricted -Command "C:\temp\install.ps1"',
+      require => File['C:/temp/install.ps1'],
+      notify  => Exec['set-sources']
   }
+
+  exec {
+    'set-sources':
+      path        => $::path,
+      command     => "C:/temp/set-sources.ps1",
+      refreshonly => true,
+      require     => File['C:/temp/set-sources.ps1']
+  } -> Package <| provider == chocolatey |>
+
 }
